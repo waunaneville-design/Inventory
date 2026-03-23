2005b03f7fe6b076fb5235f2c562d549b07eea58
Lascelles <waunaneville@gmail.com>
2026-03-23 17:33:32 +0300

    improve the code to work with jest

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -2,15 +2,6 @@
 
 
 
-// Export the necessary parts for testing
-module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
-};
-
 // Create an array to store product names
 const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
